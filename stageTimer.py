3ae7a00cd39ce24d54efd80e50778f5c680c64f4
Benedikt <benedikt@lampert.xyz>
2020-03-03 14:54:57 +0100

    Change key bindings

--- stageTimer.py
+++ stageTimer.py
@@ -18,12 +18,14 @@ app = gui("Stage timer", handleArgs=False)
 
 # Key events
 def keyPress(key):
-    if key == "<Up>":
+    if key == "Up":
         app.increaseFont()
-    elif key == "<Down>":
+    elif key == "Down":
         app.decreaseFont()
-    elif key == "<F1>":
-        os.system('sudo shutdown now')
+    elif key == "F1":
+        os.system('systemctl poweroff')
+    elif key == "Escape":
+        exit()
 
 # Window configuration
 app.setSize("Fullscreen")
@@ -37,14 +39,15 @@ app.setFg("white")
 app.addLabel("time", "00:00:00")
 
 # Widget design
-app.getLabelWidget("time").config(font="Times 300")
-
+#app.getLabelWidget("time").config(font="Times 300")
+app.setFont(size=200)
 # Run App
 app.registerEvent(runTimer)
 
-app.bindKey("<Up>", keyPress)
-app.bindKey("<Down>", keyPress)
-app.bindKey("<F1>", keyPress)
+app.bindKey("Up", keyPress)
+app.bindKey("Down", keyPress)
+app.bindKey("F1", keyPress)
+app.bindKey("Escape", keyPress)
 app.go()
 
 
